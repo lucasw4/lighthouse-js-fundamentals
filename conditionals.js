6e6c58c6f856dcd3f53112d30ba028ba5b6ded24
Lucas Sahota <lucaswade4@gmail.com>
2022-09-08 16:40:47 -0700

    Using conditionals to talk about weather

---
+++ conditionals.js
@@ -0,0 +1,19 @@
+// const raining = false;
+// const cold = false;
+//  if (cold) {
+//   console.log('Take a jacket!')
+//  } else {
+//   console.log('short sleeves are fine.')
+//  }
+
+const temperature = 12;
+
+if (temperature < 0) {
+  console.log('make sure to bring a scarf')
+} else if (temperature < 15) {
+  console.log('short sleeves wont cut it')
+} else {
+  console.log('short sleeves are fine.')
+}
+
+console.log('now youre ready to go outside!')
